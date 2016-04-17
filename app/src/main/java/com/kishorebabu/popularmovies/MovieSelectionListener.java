63e53abcaaab307b1808538f8ea9fca6076e5314
Kishore Babu <barykaed@gmail.com>
2016-04-17 19:53:11 +0530

    Basics in place

---
+++ app/src/main/java/com/kishorebabu/popularmovies/MovieSelectionListener.java
@@ -0,0 +1,1 @@
+package com.kishorebabu.popularmovies;import com.kishorebabu.popularmovies.model.Movie;/** * Created by Kishore on 4/17/2016. */public interface MovieSelectionListener {    void onMovieSelect(Movie movie);}
